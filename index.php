9deba08a5cc903f7b234218ccbaa80d68950c28c
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-01-30 18:57:52 -0300

    Implement frontend

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="shortcut icon" type="imagex/png" href="">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+
+    <!-- Bootstrap JS (com Popper incluído no bundle) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+
+    <!-- CSS do Owl Carousel -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--CSS-->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+    <!--Index-->
+    <section class="secao-index">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>
+                        <span class="adeus">Dê Adeus</span> às Dívidas de Empréstimos <span
+                            class="consignados">Consignados Hoje!</span>
+                    </h2>
+                    <h6>Dívida de consignado?</h6>
+                    <p>Livre-se das dívidas, recupere seu salário e seu crédito agora! Resolva tudo de imediato
+                        <span style="font-weight: bold;">preenchendo o formulário</span> abaixo:
+                    </p>
+
+                    <div class="formulario">
+                        <form>
+                            <label>Seu Nome: </label>
+                            <input class="form-control" type="text">
+                            <label>WhatsApp com DDD: </label>
+                            <input class="form-control" type="text">
+                            <label>E-mail: </label>
+                            <input class="form-control" type="email">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input"> <label class="form-check-label">ESTOU
+                                    CIENTE DE QUE O CUSTO MENSAL DOS SERVIÇOS É DE R$350 E POSSO
+                                    PAGAR ESSE VALOR ASSIM QUE ASSINAR O CONTRATO</label>
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <button class="btn-dividas">Sim, quero me livrar das dívidas!</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-5">
+                    <img src="./imgs/img1.png" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--Beneficios-->
+    <section class="secao-beneficios">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>A Meu Nome OK <strong>recupera seu salário</strong> e abre caminhos para <strong>novos
+                            empréstimos</strong></h2>
+                    <p class="descricao mt-5 mb-5">
+                        Com nossa expertise jurídica, transformamos a sua realidade financeira, libertando-o de dívidas
+                        opressoras.
+                    </p>
+                </div>
+                <div class="col-md-3 beneficio">
+                    <img class="img-fluid" src="./imgs/img2.webp" alt="Elimine as Dívidas">
+                    <h4>Elimine as Dívidas</h4>
+                    <p>Dê adeus às dívidas, principalmente de empréstimos consignados.</p>
+                </div>
+                <div class="col-md-3 beneficio">
+                    <img class="img-fluid" src="./imgs/img3.webp" alt="Seu Salário Intacto">
+                    <h4>Seu Salário Intacto</h4>
+                    <p>Recupere seu salário inteiro. Livre-se agora dos descontos dos consignados!</p>
+                </div>
+                <div class="col-md-3 beneficio">
+                    <img class="img-fluid" src="./imgs/img4.webp" alt="Crédito Restaurado">
+                    <h4>Crédito Restaurado</h4>
+                    <p>Recupere seus limites de crédito e melhore seu score. Tenha acesso a novos consignados.</p>
+                </div>
+                <div class="col-md-3 beneficio">
+                    <img class="img-fluid" src="./imgs/img5.webp" alt="Direito à Indenização">
+                    <h4>Direito à Indenização</h4>
+                    <p>Seja indenizado por abusos dos bancos.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Como Funciona -->
+    <section class="como-funciona">
+        <div class="container text-center">
+            <h2 class="titulo">Como funciona?</h2>
+            <div class="row justify-content-center">
+                <!-- Passo 1 -->
+                <div class="col-md-3">
+                    <i class="fas fa-user-edit icone"></i>
+                    <h4 class="passo">Passo 1</h4>
+                    <p>Informe-nos sobre suas dívidas e deixe o estresse para trás!</p>
+                </div>
+
+                <!-- Separador -->
+                <div class="col-md-1 d-none d-md-flex align-items-center">
+                    <span class="separador">-------</span>
+                </div>
+
+                <!-- Passo 2 -->
+                <div class="col-md-3">
+                    <i class="fas fa-gavel icone"></i>
+                    <h4 class="passo">Passo 2</h4>
+                    <p>Nossos advogados entram em ação, lutando judicialmente pelos seus direitos.</p>
+                </div>
+
+                <!-- Separador -->
+                <div class="col-md-1 d-none d-md-flex align-items-center">
+                    <span class="separador">-------</span>
+                </div>
+
+                <!-- Passo 3 -->
+                <div class="col-md-3">
+                    <i class="fas fa-party-horn icone"></i>
+                    <h4 class="passo">Passo 3</h4>
+                    <p>Celebre a liberdade: sem dívidas, com salário íntegro e crédito renovado!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Dra Mariana -->
+    <section class="dra-mariana">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Imagem -->
+                <div class="col-md-5 text-center mt-5">
+                    <img src="./imgs/img6.webp" alt="Dra Mariana Cunha" class="img-fluid dra-img">
+                </div>
+
+                <!-- Texto -->
+                <div class="col-md-7">
+                    <h2 class="titulo">Dra Mariana Cunha</h2>
+                    <h4 class="subtitulo">Diretora Jurídica Meu Nome OK</h4>
+                    <p class="descricao">
+                        "Eu, Mariana Cunha, e minha equipe na Meu Nome OK, estamos aqui para levar você à liberdade
+                        financeira.
+                        Vamos usar nossa expertise para zerar suas dívidas e trazer de volta sua paz de espírito!"
+                    </p>
+
+                    <a href="#" class="btn-cta">SIM! QUERO MINHA LIBERDADE FINANCEIRA!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Acabe com as Dívidas -->
+    <section class="acabe-com-as-dividas">
+        <div class="container text-center">
+            <h2 class="titulo">Acabe com as Dívidas Imediatamente!</h2>
+            <p class="subtitulo">
+                Livre-se de <strong>TODAS AS SUAS DÍVIDAS</strong> pagando
+                <span class="destaque-vermelho">apenas R$350/mês no boleto!</span>
+            </p>
+            <p class="descricao">
+                <strong>Preencha o formulário abaixo</strong> e obtenha a primeira vitória contra as dívidas. <br>
+                Responderemos em instantes!
+            </p>
+
+            <!-- Formulário -->
+            <div class="formulario-container">
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Qual seu nome?" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Qual seu WhatsApp com DDD?" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Qual seu e-mail?" required>
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="confirmacao">
+                        <label class="form-check-label" for="confirmacao">
+                            <span class="aviso-vermelho">
+                                ESTOU CIENTE DE QUE O CUSTO MENSAL DOS SERVIÇOS É DE R$350 E
+                                <strong>POSSO PAGAR</strong> ESSE VALOR ASSIM QUE ASSINAR O CONTRATO
+                            </span>
+                        </label>
+                    </div>
+                    <button type="submit" class="btn-enviar">SIM! QUERO ME LIVRAR DAS DÍVIDAS!</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <!--Sucessos dos clientes-->
+    <section class="sucessos-dos-clientes">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="titulo">Veja o Sucesso dos nossos <span style="font-weight: bold;">Clientes!</span></h2>
+                </div>
+                <div class="col-md-6 img-cliente">
+                    <img class="img-fluid" src="./imgs/cliente1.png">
+                </div>
+                <div class="col-md-6 img-cliente">
+                    <img class="img-fluid" src="./imgs/cliente2.png">
+                </div>
+                <div class="col-md-6 img-cliente">
+                    <img class="img-fluid" src="./imgs/cliente3.png">
+                </div>
+                <div class="col-md-6 img-cliente">
+                    <img class="img-fluid" src="./imgs/cliente4.png">
+                </div>
+                <div class="col-md-6 img-cliente">
+                    <img class="img-fluid" src="./imgs/cliente5.png">
+                </div>
+                <div class="col-md-6 img-cliente">
+                    <img class="img-fluid" src="./imgs/cliente6.png">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--Footer 1-->
+    <footer class="footer1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="d-flex justify-content-center">
+                        <img class="img-fluid" src="./imgs/footer1.avif">
+                    </div>
+                    <p>A mídia e milhares de clientes felizes recomendam nosso serviço!</p>
+                </div>
+                <div class="col-md-5 imgs-footer">
+                    <img class="img-fluid ms-3 mb-3" src="./imgs/footer2.avif">
+                    <img class="img-fluid ms-3 mb-3" src="./imgs/footer3.avif">
+                    <img class="img-fluid ms-3 mb-3" src="./imgs/footer4.avif">
+                    <img class="img-fluid ms-3 mb-3" src="./imgs/footer5.avif">
+                    <img class="img-fluid ms-3 mb-3" src="./imgs/footer6.avif">
+                    <img class="img-fluid ms-3 mb-3" src="./imgs/footer7.avif">
+                </div>
+
+                <div class="col-md-12">
+                    <div class="d-flex justify-content-center mt-5">
+                        <button class="btn-dividas">Sim, quero me livrar das dívidas</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!--Footer 2-->
+    <footer class="footer2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img class="img-fluid" src="./imgs/footer8.avif">
+                </div>
+                <div class="col-md-6">
+                    <small>Política de Privacidade</small>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+
+<script src="js/scripts.js"></script>
