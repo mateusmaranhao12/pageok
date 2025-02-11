dca28156bc75db15ed7902d7d3b8d18ef095bb26
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-02-11 18:18:58 -0300

    Update carousel

--- conexao.php
+++ conexao.php
@@ -1,8 +1,13 @@
 <?php
-$host = '127.0.0.1';
+/*$host = '127.0.0.1';
 $dbname = 'pageok';
 $usuario = 'root';
-$senha = '';
+$senha = '';*/
+
+$host = 'sql200.infinityfree.com'; // O host do MySQL que o InfinityFree forneceu
+$dbname = 'if0_38211464_pageok_db'; // O nome do banco de dados (geralmente é prefixado pelo usuário)
+$usuario = 'if0_38211464'; // O nome de usuário do MySQL fornecido pelo InfinityFree
+$senha = 'CjOWtScApw2N'; // A senha do MySQL (verifique no painel do InfinityFree)
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
